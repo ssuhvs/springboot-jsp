4584d0d48053c280bc0c08461304232c245c2741
sszvip <sszvip@DESKTOP-6V6VR9M>
2017-01-02 10:49:26 +0800

    test

--- springboot-jsp/src/main/java/com/lostad/app/demo/controller/MainController.java
+++ springboot-jsp/src/main/java/com/lostad/app/demo/controller/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.lostad.app.demo.models.Task;
@@ -15,25 +16,19 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 
 @Controller
+@RequestMapping("/main")
 public class MainController {
 
     @Autowired
     private TaskService taskService;
 
-  
-
-    @GetMapping(value = "/all")
+    @GetMapping(value = "/")
     public String allTasks(HttpServletRequest request){
         request.setAttribute("tasks", taskService.findAll());
         request.setAttribute("mode", "MODE_TASKS");
         return "index";
     }
 
-    @GetMapping("/new")
-    public String newTask(HttpServletRequest request){
-        request.setAttribute("mode", "MODE_NEW");
-        return "index";
-    }
     @PostMapping(value="/save")
     public String saveTask(@ModelAttribute Task task, BindingResult bindingResult, HttpServletRequest request){
         taskService.save(task);
@@ -42,7 +37,7 @@ public class MainController {
         return "index";
     }
 
-    @GetMapping("/update")
+    @PostMapping("/update")
     public String updateTask(@RequestParam long id, HttpServletRequest request){
         request.setAttribute("task", taskService.findTask(id));
         request.setAttribute("mode", "MODE_UPDATE");
